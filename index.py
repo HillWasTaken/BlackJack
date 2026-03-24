726fb1c08a839e1b3b6a75999726dc6ada6ea577
Quincy <quincy.van.den.heuvel@windesheim.nl>
2026-03-24 20:19:41 +0100

    started over

--- index.py
+++ index.py
@@ -9,18 +9,3 @@ game = True
 def getRandomCard():
     randomCard = (random.choice(test), random.choice(cardNames))
     return randomCard
-
-currentScore = 0
-def gameFunction():
-    global game
-    global currentScore
-    currentScore = 0
-    while game:
-        randomCard = getRandomCard()
-        currentScore += randomCard[0]['value']
-        print(f"{randomCard[0]['name']} of {randomCard[1]}, current score: {currentScore}")
-        if currentScore >= 21:
-            game = False
-
-while game:
-    gameFunction()
