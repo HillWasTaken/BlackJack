d72ac169a2c44813264412976a5e4416831ef489
Quincy <quincy.van.den.heuvel@windesheim.nl>
2026-03-24 17:58:03 +0100

    42

--- index.py
+++ index.py
@@ -2,8 +2,25 @@
 import random
 from cards import *
 
-#get random card from the list of cards and card names
-randomCard = (random.choice(test), random.choice(cardNames))
+#make the game loop
+game = True
 
-#print the random card
-print(f"{randomCard[0]['name']} of {randomCard[1]}")
+#get random card from the list of cards and card names
+def getRandomCard():
+    randomCard = (random.choice(test), random.choice(cardNames))
+    return randomCard
+
+currentScore = 0
+def gameFunction():
+    global game
+    global currentScore
+    currentScore = 0
+    while game:
+        randomCard = getRandomCard()
+        currentScore += randomCard[0]['value']
+        print(f"{randomCard[0]['name']} of {randomCard[1]}, current score: {currentScore}")
+        if currentScore >= 21:
+            game = False
+
+while game:
+    gameFunction()
