4aa3aede4df1782cc0fb0f13d9f082c214897133
Quincy <quincy.van.den.heuvel@windesheim.nl>
2026-03-24 21:42:45 +0100

    added function to calculate the score

--- index.py
+++ index.py
@@ -7,5 +7,21 @@ game = True
 
 #get random card from the list of cards and card names
 def getRandomCard():
-    randomCard = (random.choice(test), random.choice(cardNames))
+    randomCard = [random.choice(test), random.choice(cardNames)]
     return randomCard
+
+#get the score of the cards
+def getScore(cards):
+    total = 0
+    for card in cards:
+        cardName = card[0]['name']
+        cardValue = card[0]['value']
+        if cardName == 'Ace' and total < 21:
+            cardValue = 11
+        elif cardName == 'Ace' and total >= 21:
+            cardValue = 1
+        total += cardValue
+    return total
+
+randomCards = getRandomCard(), getRandomCard()
+print(getScore(randomCards))
