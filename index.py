816e427a99587f95390b6b6bf87aeb774e229109
Quincy <quincy.van.den.heuvel@windesheim.nl>
2026-03-24 17:24:53 +0100

    added some comments

--- index.py
+++ index.py
@@ -2,6 +2,8 @@
 import random
 from cards import *
 
+#get random card from the list of cards and card names
 randomCard = (random.choice(test), random.choice(cardNames))
 
+#print the random card
 print(f"{randomCard[0]['name']} of {randomCard[1]}")
